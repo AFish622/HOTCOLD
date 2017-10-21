34323c957242d431debd004555ec10304ed73543
Austin Fisher <AFish622@gmail.com>
2017-10-20 20:02:43 -0700

    initial commit

---
+++ src/components/game-title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './game-title.css';
+
+
+export default function GameTitle(props) {
+	return (
+			<div>
+				<h1 className="title">HOT or COLD</h1>
+			</div>
+		)
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GameCenter from './components/game-center';
+// import GameTitle from './components/game-title';
+import BigContainer from './components/big-container';
+
+ReactDOM.render(<BigContainer />, document.getElementById('root'));
